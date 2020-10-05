3fbcd1105df6c1b057a0427e4fc40e3f626212de
chmikhail <mikhail.chr@gmail.com>
2020-10-05 23:44:18 +0500

    Task3 done

---
+++ Lesson02/Task01.py
@@ -0,0 +1,4 @@
+# Делаем список
+spisok = (4, "jone", 3.15)
+for type(int) in spisok:
+    print("1")
---
+++ Lesson02/Task03.py
@@ -0,0 +1,9 @@
+mount_in = int(input("Введите номер месяца: "))
+season_list = ["зима", "весна", "лето", "осень"]
+season_dict = {0: "зима", 1: "весна", 2: "лето", 3: "осень"}
+index = (mount_in % 12) // 3
+season_spisok=season_list[index]
+season_slovar=season_dict.get(index)
+print(f"Списком:",season_spisok)
+print(f"Списком:",season_slovar)
+
