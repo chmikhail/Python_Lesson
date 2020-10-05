b0f5eb8d707dad30e86f17d7e2d0cb96d2eed38e
chmikhail <mikhail.chr@gmail.com>
2020-10-06 00:52:52 +0500

    12

---
+++ Lesson02/Task02.py
@@ -0,0 +1,6 @@
+# spisok=list(input("Введите список без пробелов:"))
+spisok = (1, "q", 2, "w", 3, "e", 4)
+count_index = int(len(spisok))
+# index_change = (count_index % 2) //
+# print(index_change)
+
