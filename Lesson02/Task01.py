eab9c704316dbf5c19b702a0613bcf7facc53c3d
chmikhail <mikhail.chr@mgail.com>
2020-10-06 12:07:04 +0500

    Task04 done

--- Lesson02/Task01.py
+++ Lesson02/Task01.py
@@ -1,6 +1,8 @@
 # Делаем список
 spisok = (4, "jone", 3.15)
+#Задаем стартовый инлекс
 start_index = 0
+# Получаем последний индекс
 end_index = len(spisok) - 1
 while start_index <= end_index:
     print(f"Индекс",start_index," имеет тип данные: ", type(spisok[start_index]))
--- Lesson02/Task04.py
+++ Lesson02/Task04.py
@@ -1,2 +1,4 @@
-spisok = list(map(input().split())
-print(spisok)
+
+spisok = list(map(str, input("Введите ваш текст: ").split()))
+for i, spisok in enumerate(spisok):
+    print(i + 1, spisok[:10])
