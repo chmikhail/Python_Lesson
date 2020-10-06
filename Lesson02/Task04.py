8dc5f1725b49f43fed154fb74d2118148e22c1c0
chmikhail <mikhail.chr@gmail.com>
2020-10-06 16:05:17 +0500

    Update task04

--- Lesson02/Task04.py
+++ Lesson02/Task04.py
@@ -1,4 +1,4 @@
-
-spisok = list(map(str, input("Введите ваш текст: ").split()))
+print("Введите ваш текст,")
+spisok = list(map(str, input("для проверки пишите текст больше 10 мисволов слово и нескоко пробелов : ").split()))
 for i, spisok in enumerate(spisok):
     print(i + 1, spisok[:10])
