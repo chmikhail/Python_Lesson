92b773c2fd0228db75c992e57b5723a3714c7edd
chmikhail <mikhail.chr@gmail.com>
2020-10-10 23:16:15 +0500

    Update Task04.py

--- Lesson03/Task04.py
+++ Lesson03/Task04.py
@@ -1,13 +1,21 @@
 def my_func(x, y):
-    y = -y
-    result = 1 / (x ** y)
+    if y < 0:
+        y = -y
+        result = 1 / (x ** y)
+    else:
+        result = (x ** y)
     return print(result)
 
+
 def my_func_task(x, y):
-    y = -y
-    result = 1
-    for _ in range(y): result *= x
-    result = 1 / result
+    if y < 0:
+        y = -y
+        result = 1
+        for _ in range(y): result *= x
+        result = 1 / result
+    else:
+        result = 1
+        for _ in range(y): result *= x
     print(result)
 
 
@@ -16,4 +24,3 @@ y = int(input("Степень: "))
 
 print(my_func(x, y))
 print(my_func_task(x, y))
-
