e4fd2c8ed707722698404bf4eced3e6c28abe250
chmikhail <mikhail.chr@gmail.com>
2020-10-15 00:31:29 +0500

    update

--- Lesson04/Task02.py
+++ Lesson04/Task02.py
@@ -3,6 +3,7 @@
 # Пример исходного списка: [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55].
 # Результат: [12, 44, 4, 10, 78, 123].
 
-spisok=[300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+spisok = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+new_spisok = [x for x in spisok if x>x in range(spisok)]
 
-new_spisok=[x for x in spisok if x>x  ]
+print(new_spisok)
